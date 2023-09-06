f8396e0f7dd76f21c3d0f66adc30b8d5c6928162
Tan Thai <Vi.Tan.Thai@gmail.com>
2023-09-06 11:01:33 +0200

    Made code my dry, pop and releasedate sorting now works.

---
+++ MovieMatchMvc/Migrations/20230906083427_updated Watchlist dbo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieMatchMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedWatchlistdbo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Popularity",
+                table: "watchLists",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "watchLists",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Popularity",
+                table: "watchLists");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "watchLists");
+        }
+    }
+}
